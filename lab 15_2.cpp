2c24837422214885cb10a61281b4cd35a339d9f5
belikmaks <belikmaks685@gmail.com>
2023-12-11 21:50:58 +0200

    Завдання 2

---
+++ lab 15_2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cstdlib>
+#define arraySize 10
+
+int main() {
+    // Оголошення масиву розміром 10 
+    int array[arraySize];
+
+    for(int i = 0; i < arraySize; i++)
+    {
+         array[i] = rand();              //Заповнення масиву
+         printf("%d\n",array[i]);
+    }
+    for (int i = 0; i < arraySize; i += 2) {
+        array[i] = 0;                          //обнулення парних елементів
+    }
+    printf("Масив після обнулення парних елементів:\n");
+    for (int i = 0; i < arraySize; ++i) {
+    printf("%d ", array[i]);                  //Виведення масиву після обнулення
+    }
+    return 0; 
+    }
