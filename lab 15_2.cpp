40c48171075d5386a40de1a4c66798bdf79124bb
belikmaks <belikmaks685@gmail.com>
2023-12-14 00:14:48 +0200

    Завдання 3

--- lab 15_2.cpp
+++ lab 15_2.cpp
@@ -1,22 +1,29 @@
 #include <stdio.h>
-#include <cstdlib>
-#define arraySize 10
-
+#include <stdlib.h>
+#include <time.h>
+#define arraySize 10 // Розмір масиву
 int main() {
-    // Оголошення масиву розміром 10 
+    srand(time(NULL)); // Встановлення seed для генератора випадкових чисел
+    // Заповнення масиву випадковими числами
     int array[arraySize];
-
-    for(int i = 0; i < arraySize; i++)
-    {
-         array[i] = rand();              //Заповнення масиву
-         printf("%d\n",array[i]);
-    }
-    for (int i = 0; i < arraySize; i += 2) {
-        array[i] = 0;                          //обнулення парних елементів
-    }
-    printf("Масив після обнулення парних елементів:\n");
     for (int i = 0; i < arraySize; ++i) {
-    printf("%d ", array[i]);                   //Виведення масиву після обнулення
+        array[i] = rand() % 100; // Генерація випадкового числа в діапазоні від 0 до 99
     }
-    return 0; 
+    printf("Початковий масив: ");    // Виведення початкового масиву
+    for (int i = 0; i < arraySize; ++i) {
+        printf("%d ", array[i]);
     }
+    printf("\n");
+    // Обнулення парних елементів
+    for (int i = 0; i < arraySize; ++i) {
+        if (array[i] % 2 == 0) {
+            array[i] = 0;
+        }
+    }
+    printf("Масив після обнулення парних елементів: ");    // Виведення результату
+    for (int i = 0; i < arraySize; ++i) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
+ 
